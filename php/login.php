867564aef353d11a4329042aabf0c4173645b556
vincent-pc <VincentNdegwa>
2023-07-28 16:33:12 +0300

    removed the echo

--- php/login.php
+++ php/login.php
@@ -5,7 +5,6 @@ if($_POST['action']=='login'){
     include "../DB.php";
     // Verify Authentication
     $query = "SELECT * FROM users WHERE username='".$_POST['username']."' AND password='".$_POST['password']."' and role='student' ";
-    echo $query;
     $execution = $connection->query($query);
     $results = $execution->fetchAll(PDO::FETCH_ASSOC);
 
@@ -17,6 +16,7 @@ if($_POST['action']=='login'){
         $_SESSION['userid'] = $results[0]['id'];
         $_SESSION['username'] = $results[0]['username'];
         $_SESSION['userid']."'";
+        
         $sql = "SELECT * FROM students WHERE userid = '".$_SESSION['userid']."' ";
         $execution = $connection->query($sql);
         $results = $execution->fetchAll(PDO::FETCH_ASSOC);
