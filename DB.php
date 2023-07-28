791f9ba86e337f723497a2187bec347e5e985c8c
Alfa Zeroh <60199092+AlfaZeroh1@users.noreply.github.com>
2023-07-28 10:15:09 +0300

    Maintain this DB.php

--- DB.php
+++ DB.php
@@ -1,9 +1,9 @@
 <?php
 // These are the credentials you'll use to connect to your database
 $server_name = "localhost";
-$mysql_user = "vincent-pc";
-$password = "vincent-pc";
-$databases_we_are_using = "scanner";
+$mysql_user = "your_mysql_username";
+$password = "your_mysql_password";
+$databases_we_are_using = "your_db";
 
 try {
     $connection = new PDO("mysql:host=$server_name;dbname=$databases_we_are_using", $mysql_user, $password);
