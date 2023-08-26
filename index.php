1351e8e2b69b8a4766a461e29406dfe47ae4cbfe
alpha <alpha@alpha.alpha>
2023-08-27 00:12:38 +0300

    warnings disabled everywhere 2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // include "DB.php";
 
 if(empty($_SESSION['userid'])){
-    header("Location: php/login.php");
+    header("Location: login.php");
 }
 // else{print_r($_SESSION);}
 // continue with your current page
